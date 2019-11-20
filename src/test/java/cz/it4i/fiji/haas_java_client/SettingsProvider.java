1ffdf0b2950cafe0feac62d98156e4febf317d8d
Jan Kožusznik <jan@kozusznik.cz>
2019-11-20 14:40:10 +0100

    code: Move SettingsProvider into tests

---
+++ src/test/java/cz/it4i/fiji/haas_java_client/SettingsProvider.java
@@ -0,0 +1,41 @@
+package cz.it4i.fiji.haas_java_client;
+
+final class SettingsProvider {
+	
+	private SettingsProvider() {}
+	
+	static HaaSClientSettings getSettings(String projectId, String configFileName) {
+		Constants constants = new Constants(configFileName);
+		return new HaaSClientSettings() {
+			
+			@Override
+			public String getUserName() {
+				return  constants.getUserName();
+			}
+			
+			@Override
+			public String getPhone() {
+				return constants.getPhone();
+			}
+			
+			@Override
+			public String getPassword() {
+				return constants.getPassword();
+			}
+			
+			@Override
+			public String getEmail() {
+				return constants.getEmail();
+			}
+
+		
+			@Override
+			public String getProjectId() {
+				return projectId;
+			}
+
+			
+		};
+	}
+	
+}
